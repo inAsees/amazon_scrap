7f45f0007bacb3e37a71ba10c12fbc410cbe5673
inAsees <gurasees.singh121@gmail.com>
2022-05-23 20:57:07 +0530

    Instance self._urls_to_scrap(holds list of urls to scrap) created inside constructor.

--- src/scraper.py
+++ src/scraper.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import List, Tuple
 import csv
-
 import requests as req
 from bs4 import BeautifulSoup as bs
 
@@ -17,15 +16,16 @@ class ProductInfo:
 class Scraper:
     def __init__(self):
         self._country_codes_and_asins = self._get_country_code_and_asin()
-
-
+        self._urls_to_scrap = ["https://www.amazon.{}/dp/{}".format(ccode, asin) for ccode, asin in
+                              self._country_codes_and_asins]
 
     @staticmethod
     def _get_country_code_and_asin() -> List[Tuple]:
         res = []
-        with open(r"C:\Users\DELL\Desktop\Credicxo\Amazon Scraping - Sheet1.csv", "r") as f:
+        with open(r"C:\Users\DELL\Desktop\Credicxo\amazon_scraping_sheet1.csv", "r") as f:
             reader = csv.reader(f)
             next(reader)
             for row in reader:
                 res.append((row[3], row[2]))
         return res
+
