e87f2615bfe966568ea5d4dbb91e15560eca692b
inAsees <gurasees.singh121@gmail.com>
2022-05-23 20:35:59 +0530

    Created class scraper , and inside it created a method __init__ 1. Created a method _get_country_code_and_asin , which will fetch all the country codes and asin's from the file and store as a List[Tuple] , this method will be called from the constructor(__init__).

--- src/scraper.py
+++ src/scraper.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from typing import List, Tuple
+import csv
+
 import requests as req
 from bs4 import BeautifulSoup as bs
 
@@ -9,3 +12,21 @@ class ProductInfo:
     product_image_url: str
     product_price: str
     product_details: str
+
+
+class Scraper:
+    def __init__(self):
+        self._country_codes_and_asins = self._get_country_code_and_asin()
+
+    def _get_country_code_and_asin(self) -> List[Tuple]:
+        res = []
+        with open(r"C:\Users\DELL\Desktop\Credicxo\Amazon Scraping - Sheet1.csv", "r") as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                res.append((row[3], row[2]))
+        return res
+
+
+if __name__ == "__main__":
+    pass
